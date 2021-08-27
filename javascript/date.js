e7162dd87124549592c87ae77fdc1296a5d9f6d9
Austinbabe <yo861015@gmail.com>
2021-08-27 22:56:15 +0800

    chore: update ForEach and date

---
+++ javascript/date.js
@@ -0,0 +1,35 @@
+// let today = new Date();
+// let month = today.getMonth() + 1;
+// console.log(month);
+// let arrayA = [1, 3, 4];
+// let arrayB = [2, 3, 1];
+// //-----
+
+// let alice = 0;
+// let bob = 0;
+// for (let i = 0; i < arrayA.length; i++) {
+//   if (arrayA[i] > arrayB[i]) {
+//     alice++;
+//   }
+//   if (arrayA[i] < arrayB[i]) {
+//     bob++;
+//   }
+// }
+// let ans = [];
+// ans.push(alice);
+// ans.push(bob);
+// console.log(ans);
+
+// function gradingStudents(grades) {
+//   // Write your code here
+//   grades.forEach((element, index) => {
+//     console.log(`第${index}個數字為:${element}`);
+//   });
+
+// for (let i = 0; i < grades.length; i++){
+//   console.log(grades[i]);
+// }
+// }
+
+// let ary1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0];
+// gradingStudents(ary1);
